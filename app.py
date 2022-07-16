b89e2c04df7a225d17322c336989978bfebb3b08
Kamal Koranga <kamalkornga75@gmail.com>
2022-07-16 05:32:27 +0530

    with js

--- app.py
+++ app.py
@@ -1,30 +1,9 @@
-from flask import *
+from flask import Flask, render_template
 app = Flask(__name__)
-import requests
-import json
-
-url = "https://ai-quote3.p.rapidapi.com/ai-quotes/"
-
-headers = {
-	"X-RapidAPI-Key": "13a3fdc433mshc472900b88d594cp11dce7jsnadce2b996b30",
-	"X-RapidAPI-Host": "ai-quote3.p.rapidapi.com"
-}
-
-response = requests.request("GET", url, headers=headers)
-
-# print(response.text)
-
-a = response.text
-b = json.loads(a)
-
-# print(type(b))
-# print(response.text)
-# print(b)
 
 @app.route('/')
 def index():
-    data = b
-    return render_template('index.html', data=b)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
